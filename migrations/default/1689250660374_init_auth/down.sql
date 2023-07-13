a5ffeeabb57297930b412aa07472014de4c71a09
Simon Schwedes <simon@schwedes.eu>
2023-07-13 15:03:59 +0200

    fix: migrations

--- migrations/default/1689250660374_init_auth/down.sql
+++
@@ -1,3 +0,0 @@
--- COMPARE this file with migrations/default/1689250660374_init_auth/up.sql
-DROP SCHEMA IF EXISTS auth CASCADE;
-
--- migrations/default/1689250760677_init_storage/down.sql
+++
@@ -1,2 +0,0 @@
--- COMPARE this file with migrations/default/1689250760677_init_storage/up.sql
-DROP SCHEMA IF EXISTS storage CASCADE;
